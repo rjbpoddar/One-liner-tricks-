c1548e60c4cc5a154cafeaad70250b72ffdfa59b
Rajib Poddar <97610040+rjbpoddar@users.noreply.github.com>
2024-01-31 02:44:08 +0530

    Create calculator.py
    two line code

---
+++ calculator.py
@@ -0,0 +1,12 @@
+equation = input("Enter the expression : ")
+print(eval(equation))
+'''
+equation = input("Enter the expression : "): This line prompts the user to enter a mathematical expression. 
+The input function is used to take user input, and the message "Enter the expression : " is displayed as a prompt.
+Whatever the user types in response is stored in the variable equation.
+print(eval(equation)): This line evaluates the expression entered by the user and prints the result. Here's the breakdown:
+
+eval() is a built-in Python function that evaluates a string as a Python expression. In this case, it takes the user's input stored in the variable equation.
+
+The result of eval(equation) is then printed using the print function.
+'''
